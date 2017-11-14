130db77296f3f77215b8394e781182dfdf629c12
xiaopc <jeiker@126.com>
2017-11-14 20:32:48 +0800

    add swagger token header
    add swagger token header

--- src/main/java/com/jeiker/mall/config/Swagger2Config.java
+++ src/main/java/com/jeiker/mall/config/Swagger2Config.java
@@ -4,13 +4,19 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.ParameterBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.schema.ModelRef;
 import springfox.documentation.service.ApiInfo;
-//import springfox.documentation.service.Contact;
+import springfox.documentation.service.Contact;
+import springfox.documentation.service.Parameter;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * @author : xiao
@@ -24,8 +30,20 @@ public class Swagger2Config {
 
     @Bean
     public Docket createRestApi() {
+        ParameterBuilder tokenPar = new ParameterBuilder();
+        List<Parameter> parameters = new ArrayList<>();
+        tokenPar.name("xmall-Token")
+                .description("token")
+                .defaultValue("admin")
+                .modelRef(new ModelRef("string"))
+                .parameterType("header")
+                .required(false)
+                .build();
+        parameters.add(tokenPar.build());
         Docket docket = new Docket(DocumentationType.SWAGGER_2)
                 .apiInfo(apiInfo())
+                .useDefaultResponseMessages(false)
+                .globalOperationParameters(parameters)
                 .select()
                 .apis(RequestHandlerSelectors.basePackage("com.jeiker.mall.controller"))
                 .paths(PathSelectors.any())
@@ -38,7 +56,7 @@ public class Swagger2Config {
                 .title("x-mall 商城项目")
                 .license("MIT")
                 .licenseUrl("http://opensource.org/licenses/MIT")
-                .contact("jeiker@126.com")
+                .contact(new Contact("jeiker", "jeiker.cn", "jeiker@126.com"))
                 .version("1.0")
                 .build();
         return apiInfo;
